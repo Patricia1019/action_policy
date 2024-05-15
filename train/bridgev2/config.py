fd9e9848007b8aaa0a204e79a685c405c54271e0
Patricia1019 <peiqiyu1019@gmail.com>
2024-05-15 10:34:49 +0800

    fundamental framework for training

---
+++ train/bridgev2/config.py
@@ -0,0 +1,168 @@
+import argparse
+
+DATASETS = [
+    "/home/yupeiqi/bridge_data_v2/raw_pkl"
+]
+
+ACT_MEAN = [
+    1.9296819e-04,
+    1.3667766e-04,
+    -1.4583133e-04,
+    -1.8390431e-04,
+    -3.0808983e-04,
+    2.7425270e-04,
+    5.9716219e-01,
+]
+
+ACT_STD = [
+    0.00912848,
+    0.0127196,
+    0.01229497,
+    0.02606696,
+    0.02875283,
+    0.07807977,
+    0.48710242,
+]
+
+GOAL_RELABELING_KWARGS = dict(reached_proportion=0.0)
+
+AUGMENT_KWARGS = dict(
+    random_resized_crop=dict(
+        size=[128, 128],
+        scale=[0.8, 1.0],
+        ratio=[0.9, 1.1],
+        antialias=True
+    ),
+    color_jitter=dict(
+        brightness=0.2,
+        contrast=[0.8, 1.2],
+        saturation=[0.8, 1.2],
+        hue=0.1,
+    ),
+    augment_order=[
+        "random_resized_crop",
+        "color_jitter",
+    ],
+)
+
+ENCODER_KWARGS = dict(
+    pooling_method="avg",
+    add_spatial_coordinates=True,
+    act="SiLU",
+    input_img_shape=[128, 128],
+    input_channels=3
+)
+
+def get_args():
+    def str2bool(v):
+        return v.lower() in ('true')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local_rank', type=int, default=-1, help='local rank passed from distributed launcher')
+    
+    data_arg = parser.add_argument_group('Data')
+    data_arg.add_argument('--sample_weights', type=str, default='balance')
+    data_arg.add_argument('--num_workers', type=int, default=8)
+    data_arg.add_argument('--relabel_actions', type=str2bool, default=True)
+    data_arg.add_argument('--goal_relabeling_strategy', type=str, default='uniform')
+    data_arg.add_argument('--augment', type=str2bool, default=False)
+
+    model_arg = parser.add_argument_group('Model')
+    model_arg.add_argument('--dtype', type=str, default='fp32')
+    model_arg.add_argument('--encoder', type=str, default='resnetv1-34-bridge')
+    model_arg.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
+    model_arg.add_argument('--env', type=str, default='hopper')
+    model_arg.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
+    model_arg.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
+    model_arg.add_argument('--max_length', type=int, default=20)
+    model_arg.add_argument('--pct_traj', type=float, default=1.)
+    model_arg.add_argument('--embed_dim', type=int, default=128)
+    model_arg.add_argument('--n_layer', type=int, default=3)
+    model_arg.add_argument('--n_head', type=int, default=1)
+    model_arg.add_argument('--activation_function', type=str, default='relu')
+    model_arg.add_argument('--num_steps_per_iter', type=int, default=10000)
+    model_arg.add_argument('--state_dim', type=int, default=128) ###
+    model_arg.add_argument('--act_dim', type=int, default=7)
+    model_arg.add_argument('--max_ep_len', type=int, default=1000)
+    model_arg.add_argument('--dropout', type=float, default=0.1)
+
+    learn_arg = parser.add_argument_group('Learning')
+    learn_arg.add_argument('--save_dir', type=str, default='decision_transformer_save_ckpt')
+    learn_arg.add_argument('--ckpt_id', type=str, default=None)
+    learn_arg.add_argument('--train_batch_size', type=int, default=256)
+    learn_arg.add_argument('--gradient_accumulation_steps', type=int, default=1)
+    learn_arg.add_argument('--eval_batch_size', type=int, default=256)
+    learn_arg.add_argument('--max_lr', type=float, default=3e-4)
+    learn_arg.add_argument('--min_lr', type=float, default=1e-5)
+    learn_arg.add_argument('--weight_decay', type=float, default=1e-6)
+    learn_arg.add_argument('--max_grad_norm', type=float, default=5.0)
+    learn_arg.add_argument('--epochs', type=int, default=None)
+    learn_arg.add_argument('--steps', type=int, default=300000)
+    learn_arg.add_argument('--warmup_steps', type=int, default=10000)
+    learn_arg.add_argument('--decay_steps', type=int, default=None)
+    learn_arg.add_argument('--log_interval', type=int, default=5000)
+    learn_arg.add_argument('--eval_interval', type=int, default=10000)
+    learn_arg.add_argument('--save_interval', type=int, default=10000)
+    learn_arg.add_argument('--save_best', type=str2bool, default=True)
+    learn_arg.add_argument('--main_metric', type=str, default='log_probs')
+    
+    misc_arg = parser.add_argument_group('MISC')
+    misc_arg.add_argument('--method', type=str, default='df')
+    misc_arg.add_argument('--random_seed', type=int, default=42)
+
+    args, unparsed = parser.parse_known_args()
+    if len(unparsed) > 1:
+        print("Unparsed args: {}".format(unparsed))
+
+    args.datasets = DATASETS
+    args.act_mean = ACT_MEAN
+    args.act_std = ACT_STD
+    args.goal_relabeling_kwargs = GOAL_RELABELING_KWARGS
+    args.augment_kwargs = AUGMENT_KWARGS
+    args.encoder_kwargs = ENCODER_KWARGS
+
+    return args
+
+
+def get_ds_config(args):
+    ds_config = {
+        "train_batch_size": args.train_batch_size,
+        "gradient_accumulation_steps": args.gradient_accumulation_steps,
+        "steps_per_print": args.log_interval,
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "weight_decay": args.weight_decay,
+            }
+        },
+        "scheduler": {
+            "type": "WarmupDecayLR",
+            "params": {
+                "total_num_steps": args.steps if args.steps else args.warmup_steps + args.decay_steps,
+                "warmup_min_lr": args.min_lr,
+                "warmup_max_lr": args.max_lr,
+                "warmup_num_steps": args.warmup_steps,
+            }
+        },
+        "gradient_clipping": args.max_grad_norm,
+        "bf16": {
+            "enabled": args.dtype == "bf16"
+        },
+        "fp16": {
+            "enabled": args.dtype == "fp16",
+            "fp16_master_weights_and_grads": False,
+            "loss_scale": 0,
+            "loss_scale_window": 500,
+            "hysteresis": 2,
+            "min_loss_scale": 1,
+            "initial_scale_power": 15
+        },
+        # "zero_optimization": {
+        #     "stage": 2,
+        #     "contiguous_gradients": True,
+        #     "overlap_comm": True,
+        #     "reduce_scatter": True,
+        # }
+    }
+
+    return ds_config
---
+++ train/bridgev2/train_debug.py
@@ -0,0 +1,248 @@
+import os,sys
+import json
+FILE_PATH = os.path.dirname(__file__)
+
+import torch
+import torch.distributed as dist
+import deepspeed
+from deepspeed.utils import RepeatingLoader
+
+from config import get_args, get_ds_config
+from utils.misc import set_seed
+from utils.criterion import action_criterion
+from encoder.resnet import resnetv1_configs
+sys.path.append(os.path.join(FILE_PATH,'..','..'))
+from datasets.bridgev2.bridge_dataset import BridgeDatasetTorch
+from decision_transformer.models.decision_transformer import DecisionTransformer
+
+import pdb
+
+def evaluate(args, engine, eval_dataset, criterion):
+    rank = args.local_rank
+
+    eval_dataloader = torch.utils.data.DataLoader(eval_dataset, batch_size=args.eval_batch_size, num_workers=args.num_workers)
+
+    log_probs, mse, pi_actions, cnt = 0, 0, 0, 0
+    with torch.no_grad():
+        for data in eval_dataloader:
+            (obs_imgs, next_obs_imgs, goal_imgs, actions) = data
+
+            obs_imgs = obs_imgs.to(engine.device)
+            goal_imgs = goal_imgs.to(engine.device)
+            actions = actions.to(engine.device)
+
+            outputs = engine(obs_imgs, goal_imgs)
+            _, info = criterion(outputs, actions)
+
+            log_probs += info['log_probs']
+            mse += info['mse']
+            pi_actions += info['pi_actions']
+            cnt += 1
+
+    scores = {
+        "log_probs": log_probs/cnt,
+        "mse": mse/cnt,
+        "pi_actions": pi_actions/cnt
+    }
+
+    print(scores, f"[Local Rank]: {rank}")
+
+    return scores
+    
+
+def train(args, engine, criterion, train_dataset, eval_dataset, start_step=0):
+# def train(args, criterion, train_dataset, eval_dataset, start_step=0):
+    rank = args.local_rank
+    # world_size = engine.world_size
+    # gradient_accumulation_steps = engine.gradient_accumulation_steps()
+    # micro_train_batch_size = engine.train_micro_batch_size_per_gpu()
+    # assert micro_train_batch_size == args.train_batch_size // gradient_accumulation_steps // world_size
+    world_size=1
+    gradient_accumulation_steps=1
+    micro_train_batch_size = args.train_batch_size // gradient_accumulation_steps // world_size
+    
+    if args.steps:
+        epochs = 1
+    else:
+        epochs = args.epochs
+    
+    epoch, micro_step, step = 0, 0, 0
+    avg_loss = 0.0
+    best_score, best_step = -1e10, -1
+    for epoch in range(epochs):
+        # shuffle train dataset at the begining of each epoch
+        train_dataset.shuffle(seed=args.random_seed+epoch)
+
+        dataloader_ori = torch.utils.data.DataLoader(train_dataset, batch_size=micro_train_batch_size, num_workers=args.num_workers)
+        # allow for infinite iteration
+        if args.steps:
+            dataloader = RepeatingLoader(dataloader_ori)
+
+        for data in dataloader: # len of dataset: 44037, len of dataloader: 173
+            pdb.set_trace()
+            # print(data)
+
+            # data：obs_img, next_obs_img, goal_img, action
+            micro_step += 1
+            if micro_step % gradient_accumulation_steps == 0:
+                step += 1
+            
+            # skip previous steps to resume training
+            # This implementation does not give perfect restoration,
+            # due to the inner randomness of the model forward process
+            if (
+                step < start_step or
+                step == start_step and micro_step % gradient_accumulation_steps == 0
+            ):
+                continue
+
+            (obs_imgs, obs_pros, next_obs_imgs, next_obs_pros, actions, terminals, truncates, goal_imgs, goal_pros, rewards, masks) = data
+            
+            # obs_imgs = obs_imgs.to("cuda")
+            # goal_imgs = goal_imgs.to("cuda")
+            # actions = actions.to("cuda")
+
+            pdb.set_trace()
+            states = obs_imgs
+            actions = actions
+            rewards = rewards
+            # outputs = engine(obs_imgs, goal_imgs)
+            state_preds, action_preds, reward_preds = engine(states, actions, rewards, returns_to_go=rewards,timesteps=10) #############TODO
+            loss, info = criterion(outputs, actions)
+            engine.backward(loss)
+            engine.step()
+
+            avg_loss += info['actor_loss']
+
+            if micro_step % gradient_accumulation_steps != 0:
+                continue
+
+            # logging
+            if rank == 0 and step % args.log_interval == 0:
+                print(f'[Step {step}] loss: {avg_loss/args.log_interval/gradient_accumulation_steps}', flush=True)
+                avg_loss = 0.0
+
+            # evaluate
+            if step % args.eval_interval == 0 and eval_dataset:
+                if rank == 0:
+                    print(f'[Step {step}] evaluating...', flush=True)
+                scores = evaluate(args, engine, eval_dataset, criterion)
+                is_best = False
+                if scores[args.main_metric] >= best_score:
+                    best_score = scores[args.main_metric]
+                    best_step = step
+                    is_best = True
+
+            # save checkpoint
+            if step % args.save_interval == 0 and args.save_dir:
+                if args.save_best:
+                    assert args.save_interval % args.eval_interval == 0
+                    assert eval_dataset
+                    do_save = is_best
+                else:
+                    do_save = True
+
+                if do_save:
+                    client_state = {'step': step}
+                    ckpt_id = step
+                    engine.save_checkpoint(args.save_dir, ckpt_id, client_state=client_state)
+
+            # finish all training steps
+            if args.steps and step >= args.steps:
+                break
+
+        dist.barrier()
+
+    # evaluate at last
+    if step % args.eval_interval != 0 and eval_dataset:
+        if rank == 0:
+            print(f'[Step {step}] evaluating...', flush=True)
+        scores = evaluate(args, engine, eval_dataset, criterion)
+        is_best = False
+        if scores[args.main_metric] >= best_score:
+            best_score = scores[args.main_metric]
+            best_step = step
+            is_best = True
+
+    # save checkpoint at last
+    if step % args.save_interval != 0 and args.save_dir:
+        if args.save_best:
+            assert eval_dataset
+            do_save = is_best
+        else:
+            do_save = True
+
+        if do_save:
+            client_state = {'step': step}
+            ckpt_id = step
+            engine.save_checkpoint(args.save_dir, ckpt_id, client_state=client_state)
+
+    # Finish training
+    print(f'Achieve best {args.main_metric} {best_score} on evaluation set at step {best_step}.')
+
+
+
+if __name__ == '__main__':
+    args = get_args()
+    ds_config = get_ds_config(args)
+    set_seed(args.random_seed)
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    print(args)
+    if args.local_rank == 0:
+        config = {k:v for k, v in vars(args).items() if k != 'local_rank'}
+        with open(os.path.join(args.save_dir, 'config.json'), 'w') as f:
+            json.dump(config, f, ensure_ascii=False, indent=2)
+
+    # deepspeed.init_distributed()
+    rank = args.local_rank
+    # world_size = dist.get_world_size()
+
+    # create agent
+    if args.method == 'df':
+        encoder = resnetv1_configs[args.encoder](**args.encoder_kwargs)
+        agent = DecisionTransformer(
+            img_encoder=encoder,
+            state_dim=args.state_dim,
+            act_dim=args.act_dim,
+            max_length=args.max_length,
+            max_ep_len=args.max_ep_len,
+            hidden_size=args.embed_dim,
+            n_layer=args.n_layer,
+            n_head=args.n_head,
+            n_inner=4*args.embed_dim,
+            activation_function=args.activation_function,
+            n_positions=1024,
+            resid_pdrop=args.dropout,
+            attn_pdrop=args.dropout,
+        )
+    else:
+        raise NotImplementedError
+
+    # engine, _, dataloader, _ = deepspeed.initialize(
+    #     args=args,
+    #     model=agent,
+    #     model_parameters=[p for p in agent.parameters() if p.requires_grad],
+    #     config=ds_config
+    # )
+    engine = agent
+
+    # get iterable dataset
+    trainset = BridgeDatasetTorch(args, args.local_rank, world_size=1, is_train=True)
+    # do not conduct sharding on evaluation dataset, fool every proccess as it is main proccess
+    testset = BridgeDatasetTorch(args, local_rank=0, world_size=1, is_train=False)
+
+    # create loss function
+    criterion = action_criterion
+    
+    # load checkpoint if it exists,
+    # to resume training
+    start_step = 0
+    if args.save_dir and args.ckpt_id:
+        _, client_state = engine.load_checkpoint(args.save_dir, args.ckpt_id)
+        start_step = client_state['step']
+
+    # train (and also evaluate periodically)
+    # To skip training and evalute directly, just set args.epochs=0
+    # train(args, engine, criterion, trainset, testset, start_step)
+    train(args, engine, criterion, trainset, testset, start_step)
